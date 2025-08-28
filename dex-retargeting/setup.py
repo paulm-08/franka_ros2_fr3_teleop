51fa3c00a67f2ccb4ad21dac378bc36425553eee
Maestre Paul <maestre.paul.antoine.q8@dc.tohoku.ac.jp>
2025-08-28 20:44:14 +0900

    Added assets

--- dex-retargeting/setup.py
+++ dex-retargeting/setup.py
@@ -1,22 +1,18 @@
 from setuptools import setup, find_packages
 
-# We install two python packages:
-#   - dex_retargeting        (already lives under src/dex_retargeting)
-#   - dex_retargeting_ext    (we'll create for example helpers like single_hand_detector)
-
 setup(
     name="dex_retargeting",
     version="0.5.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    include_package_data=True,
-    install_requires=[
-        # Keep light; ROS apt provides numpy/scipy typically. Adjust if you want pip-driven deps.
-        # "numpy>=2.0.0", "scipy>=1.11.0", "nlopt>=2.8.0", "pin>=3.3.1", "pytransform3d>=3.5.0",
-    ],
+    include_package_data=True,  # important for MANIFEST.in to work
+    package_data={
+        "dex_retargeting": ["assets/**/*"],  # include all files recursively
+    },
+    install_requires=[],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/dex_retargeting"]),
         ("share/dex_retargeting", ["package.xml"]),
     ],
-    zip_safe=True,
 )
+zip_safe=False,
